fb4ece0845019b8df759732345e26f3cb7c4277b
huangxiaobin <huangxiaobin@cmss.chinamobile.com>
2018-01-18 09:31:26 +0800

    Initial Commit

---
+++ blocks/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  blocks
+//
+//  Created by huangxiaobin on 18/01/2018.
+//  Copyright © 2018 huangxiaobin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	}
+	return 0;
+}
